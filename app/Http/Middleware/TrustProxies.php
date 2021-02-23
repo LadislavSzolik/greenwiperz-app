3aafe236b2cb772eaa5932ab138e0a0db137b1c2
Imre Szeness <imre@prstart.co.uk>
2021-02-23 21:56:48 +0100

    traefik

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = '193.31.27.135';
 
     /**
      * The headers that should be used to detect proxies.
